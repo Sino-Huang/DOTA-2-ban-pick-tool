88d57a75c4224583d5bd7f0d83be68d2c24c3a92
Sukai Huang <hsk6808065@163.com>
2023-10-20 09:18:58 +1100

    ready to deploy

--- frontend/core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'core/index.html')
-
-def hero_grid(request):
-    return render(request, 'core/hero_grid.html')
