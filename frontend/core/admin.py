f14992ea81fe7ce8b6eba28f3c433c3ed29b7d7f
TonyDS2022 <tony.zeng.tianzhi@gmail.com>
2023-10-23 00:31:05 +0800

    🏗 Frontend: Add Attribute as a table

--- frontend/core/admin.py
+++ frontend/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from core.models import Hero
+from core.models import Attribute, Hero
 
+admin.site.register(Attribute)
 admin.site.register(Hero)
+
 # Register your models here.
--- frontend/core/migrations/0002_ingest_heroes.py
+++ frontend/core/migrations/0002_ingest_heroes.py
@@ -5,19 +5,33 @@ from pathlib import Path
 import pandas as pd
 import os
 
+def ingest_attributes(apps, schema_editor):
+    attributes = [
+        ('Strength', 1, 'hero_strength.png'),
+        ('Agility', 2, 'hero_agility.png'),
+        ('Intelligence', 3, 'hero_intelligence.png'),
+        ('Universal', 4, 'hero_universal.png'),
+    ]
+    target = apps.get_model("core", "Attribute")
+    for attribute in attributes:
+        target.objects.create(
+            name=attribute[0],
+            order=attribute[1],
+            image=os.path.join('attribute_images', attribute[2]),
+        )
+
 
 def ingest_heroes(apps, schema_editor):
     BASEPATH = Path(__file__).resolve().parent.parent.parent.parent
     resource_path = os.path.join(BASEPATH, "Valve_resources", "hero_wide_icons")
     df = pd.read_csv(os.path.join(resource_path, "names.csv"), header=0, index_col=None)
-    print(df.columns)
-    df['image'] = [resource_path + x + ".png" for x in df['alt_name']]
+    df['image'] = [os.path.join('hero_images', x + '.png') for x in df['alt_name']]
     target = apps.get_model("core", "Hero")
     for index, row in df.iterrows():
         target.objects.create(
             name=row['name'],
             alt_name=row['alt_name'],
-            primary_attribute=row['primary_attribute'],
+            primary_attribute_id=row['primary_attribute'],
             image=row['image'],
         )
 
@@ -28,5 +42,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(ingest_attributes),
         migrations.RunPython(ingest_heroes),
     ]
